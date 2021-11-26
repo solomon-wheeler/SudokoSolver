6b75f2ab89b754139ed45276403ecb47b8774832
Solly <solly.wheeler@gmail.com>
2021-11-26 13:06:02 +0000

    fixing loop but is_valid() still not working

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ class sudoku_board():
     def get_board(self):
         return self.board
     def check_solved(self):
-        for y_val in range(0, self.y_size -1 ):
-            for x_val in range(0, self.x_size -1 ):
+        for y_val in range(0, self.y_size):
+            for x_val in range(0, self.x_size):
                 if self.board[y_val][x_val] == 0:
                     return False
         return True
@@ -33,12 +33,12 @@ class sudoku_board():
         row_vals = []
         col_vals = []
         square_vals = []
-        for x_val in range (0,self.x_size -1): #checking whether the row is valid
+        for x_val in range (0,self.x_size ): #checking whether the row is valid
             this_val = self.board[self.square_changed[0], x_val]
             if this_val in row_vals: #todo could be made more efficent?
                 return False # same values in row so this state invalid
             row_vals.append(this_val)
-        for y_val in range (0,self.y_size -1): #checking whether the column is valid
+        for y_val in range (0,self.y_size): #checking whether the column is valid
             this_val = self.board[y_val, self.square_changed[1]]
             if this_val in col_vals:  # todo could be made more efficent?
                 return False #same values in column so this sate is invalid
@@ -64,45 +64,41 @@ class sudoku_board():
                     return False #same values in sqaure so this state is invalid
                 square_vals.append(this_val)
         return True
+
     def find_empty(self): #startin top left of the sudoko board looks through to find an empty square
-        for y in range(0, self.x_size - 1):
-            for x  in range(0, self.y_size -1):
+        for y in range(0, self.x_size ):
+            for x  in range(0, self.y_size ):
                 if self.board[y][x] == 0:
                     return(y,x)
         return False
     def create_new(self,value): # remeber x_location,y_location needs to be 0 indexed.
         locations = self.find_empty()
         if locations == False:
+            print("something has gone wrong here, we have found a full board, looks like this:")
+            self.print_board()
             return False
         new_board = self.board
         new_board[locations[0]][locations[1]] = value
         return sudoku_board(new_board,locations)
 
-class Solver():
-    def __init__(self):
-        self.states_generate = 0
-        self.prior_states = None #need to implement?
 
-    def go_for_this_square(self, next_state):
-        next_state.print_board()
-        if next_state.check_solved() == True:
-            return True
 
-        for new_value_to_try in range(0, 9):
-                next_state = next_state.create_new(new_value_to_try)
-                if next_state.is_valid():
-                    current_state = next_state
-                    self.go_for_this_square(current_state)
+def go_for_this_square(next_state):
+    next_state.print_board()
+    if next_state.check_solved() == True:
+        return True
 
-        return False
+    for new_value_to_try in range(0, 9):
+            next_state = next_state.create_new(new_value_to_try)
+            if next_state.check_solved():
+                return next_state
+            if next_state.is_valid():
+                lower_state = go_for_this_square(next_state)
+                if lower_state is not False and lower_state.check_solved():
+                    return lower_state
+    return False
 
-    def solve(self,initial_state):
 
-        final_value = self.go_for_this_square(initial_state)
-        if self.current_state.is_valid() == True:
-            return self.current_state.get_board()
-        else:
-            print("invaliddddd") #todo do fancy thing with the board here so that is has [-1][-1]
 
 
 
@@ -110,5 +106,5 @@ class Solver():
 
 ## main
 initital_sudoku = sudoku_board(sudoku[0],[0,0])  # here we are creating the sudoko board class for our first board, and parsing in the numpy array of our first board
-solver = Solver()
-print(solver.solve(initital_sudoku))
+returned_val = go_for_this_square(initital_sudoku)
+returned_val.print_board()
